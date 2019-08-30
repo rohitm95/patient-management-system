8c62be6a0f534677efa21010c63bddec8b11522b
rohitm95 <rohitm95@gmail.com>
2019-08-30 12:15:01 +0530

    UI change in success dialog

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -19,7 +19,7 @@ export class HeaderComponent implements OnInit {
     	this.router.navigate(['/login']);
   	}
 
-    setLogout(){
+    setLogout() {
       sessionStorage.removeItem('loggedIn');
     }
 
